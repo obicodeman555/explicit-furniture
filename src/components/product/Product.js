e4bef81ed1971ccae481b1d714e2d7e2580c4426
Obicodeman555 <obicodeman@yahoo.com>
2022-02-08 12:38:17 +0100

    design: featured products on homepage with hover effect of box-shadow on the card

--- src/components/product/Product.js
+++ src/components/product/Product.js
@@ -1,23 +1,24 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { formatPrice } from "../../utils/helpers";
+import "./product.scss";
 
 const Product = ({ image, name, price, id }) => {
   return (
-    <article>
-      <div className="container">
-        <div>
+    <div className="product">
+      <div className="product__container">
+        <div className="product__imageBox">
           <img src={image} alt="" />
         </div>
-        <div>
-          <span>{name}</span>
-          <span>{formatPrice(price)}</span>
+        <div className="product__price">
+          <span className="price__name">{name}</span>
+          <span className="price__amount">{formatPrice(price)}</span>
         </div>
-        <div>
+        <div className="product__details--cta">
           <Link to={`products/${id}`}>Product details</Link>
         </div>
       </div>
-    </article>
+    </div>
   );
 };
 
